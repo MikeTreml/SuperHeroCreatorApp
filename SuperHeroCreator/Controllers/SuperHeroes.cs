dd2f8cc8f348d4bcbd2884839904e60c2f3cb8cb
mike treml <miketreml@mac.com>
2020-10-16 12:47:26 -0500

    Created a new controller since the old one wasn't working.

--- SuperHeroCreator/Controllers/SuperHeroes.cs
+++ SuperHeroCreator/Controllers/SuperHeroes.cs
@@ -9,34 +9,38 @@ using SuperHeroCreator.Models;
 
 namespace SuperHeroCreator.Controllers
 {
-    public class SuperHeroController : Controller
+    public class SuperHeroes : Controller
     {
-        private ApplicationDbContext _db;
 
-        public SuperHeroController(ApplicationDbContext db)
+        private readonly ApplicationDbContext _db;
+        public SuperHeroes(ApplicationDbContext db)
         {
             _db = db;
         }
 
-        // GET: SuperHeroController
+
+        // GET: SuperHeroes
         public ActionResult Index()
         {
-            return View();
+            var superHeroes = _db.SuperHeroes;
+            return View(superHeroes);
         }
 
-        // GET: SuperHeroController/Details/5
+        // GET: SuperHeroes/Details/5
         public ActionResult Details(int id)
         {
-            return View();
+            var superHeroes = _db.SuperHeroes.Where(a => a.Id == id).FirstOrDefault();
+            return View(superHeroes);
         }
 
-        // GET: SuperHeroController/Create
+        // GET: SuperHeroes/Create
         public ActionResult Create()
         {
-            return View();
+            SuperHero superHero = new SuperHero();
+            return View(superHero);
         }
 
-        // POST: SuperHeroController/Create
+        // POST: SuperHeroes/Create
         [HttpPost]
         [ValidateAntiForgeryToken]
         public ActionResult Create(SuperHero superHero)
@@ -53,19 +57,22 @@ namespace SuperHeroCreator.Controllers
             }
         }
 
-        // GET: SuperHeroController/Edit/5
+        // GET: SuperHeroes/Edit/5
         public ActionResult Edit(int id)
         {
+            var editHero = _db.SuperHeroes.Where(h => h.Id == id);
             return View();
         }
 
-        // POST: SuperHeroController/Edit/5
+        // POST: SuperHeroes/Edit/5
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Edit(int id, IFormCollection collection)
+        public ActionResult Edit(int id, SuperHero superhero)
         {
             try
             {
+                _db.SuperHeroes.Update(superhero);
+                _db.SaveChanges();
                 return RedirectToAction(nameof(Index));
             }
             catch
@@ -74,19 +81,22 @@ namespace SuperHeroCreator.Controllers
             }
         }
 
-        // GET: SuperHeroController/Delete/5
+        // GET: SuperHeroes/Delete/5
         public ActionResult Delete(int id)
         {
-            return View();
+            var superHero = _db.SuperHeroes.Where(h => h.Id == id);
+            return View(superHero);
         }
 
-        // POST: SuperHeroController/Delete/5
+        // POST: SuperHeroes/Delete/5
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Delete(int id, IFormCollection collection)
+        public ActionResult Delete(int id, SuperHero superHero)
         {
             try
             {
+                _db.SuperHeroes.Remove(superHero);
+                _db.SaveChanges();
                 return RedirectToAction(nameof(Index));
             }
             catch
