00638c6db5fb1a4f64f38d2e3a77dc27c0b2a8f4
mike treml <miketreml@mac.com>
2020-10-16 10:00:12 -0500

    intial

---
+++ SuperHeroCreator/Data/Migrations/20201015200627_intial.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SuperHeroCreator.Data.Migrations
+{
+    public partial class intial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "SuperHeroes",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(nullable: true),
+                    AlterEgo = table.Column<string>(nullable: true),
+                    PrimaryAbility = table.Column<string>(nullable: true),
+                    SecondaryAbility = table.Column<string>(nullable: true),
+                    CatchPhrase = table.Column<string>(nullable: true),
+                    Picture = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_SuperHeroes", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "SuperHeroes");
+        }
+    }
+}
